91aab92b7356ccc875dc7d0ae70d98243c44d2f2
MossPipat <pt_moss17782@hotmail.com>
2024-01-06 03:30:04 +0700

    chore:modify style

--- src/component/QRCode/QRCode.tsx
+++ src/component/QRCode/QRCode.tsx
@@ -6,29 +6,31 @@ const QRCode = () => {
       <div
         style={{
           backgroundColor: "white",
+          width: "auto",
           padding: "30px",
           borderRadius: "32px",
-          boxShadow: "4px 4px 24px black",
+          boxShadow: "0px 3px 4px black",
         }}
       >
-        <img
-          style={{ borderRadius: "18px" }}
-          alt="qrCode"
-          src="../src/assets/image-qr-code.png"
-        />
         <div
           style={{
-            maxWidth: "400px",
+            width: "320px",
             margin: "auto",
-            padding: "0px 0px",
           }}
         >
-          <h1 style={{ fontSize: "32px" }}>
+          <img
+            style={{ borderRadius: "18px" }}
+            alt="qrCode"
+            src="../src/assets/image-qr-code.png"
+            width="320px"
+          />
+
+          <h1 style={{ fontSize: "24px" }}>
             Improve your front-end skills by building projects
           </h1>
           <p
             style={{
-              fontSize: "24px",
+              fontSize: "20px",
               color: "slategrey",
               fontStyle: "normal",
             }}
